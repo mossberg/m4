f72b93a6bd966605c41f7f77b91430f38986ee0b
Mark Mossberg <mark.mossberg@gmail.com>
2016-02-18 12:18:27 -0500

    stuff

---
+++ src/pic16_usound/main.c
@@ -0,0 +1,45 @@
+/******************************************************************************/
+/* Files to Include                                                           */
+/******************************************************************************/
+
+#if defined(__XC)
+    #include <xc.h>         /* XC8 General Include File */
+#elif defined(HI_TECH_C)
+    #include <htc.h>        /* HiTech General Include File */
+#endif
+
+#include <stdint.h>        /* For uint8_t definition */
+#include <stdbool.h>       /* For true/false definition */
+
+#include "system.h"        /* System funct/params, like osc/peripheral config */
+#include "user.h"          /* User funct/params, such as InitApp */
+
+#define _XTAL_FREQ 8000000
+/******************************************************************************/
+/* User Global Variable Declaration                                           */
+/******************************************************************************/
+
+/* i.e. uint8_t <variable_name>; */
+
+/******************************************************************************/
+/* Main Program                                                               */
+/******************************************************************************/
+void main(void)
+{
+    /* Configure the oscillator for the device */
+    ConfigureOscillator();
+
+    /* Initialize I/O and Peripherals for application */
+    InitApp();
+    TRISC0 = 0;
+    while(1)
+    {
+        RC0 = 1;
+        __delay_ms(500);  // 1 second delay 
+    RC0=0;                    // make RD7 pin Low to Off LED 
+    __delay_ms(500);
+        /* TODO <INSERT USER APPLICATION CODE HERE> */
+    }
+
+}
+
