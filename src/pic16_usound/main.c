4c47b9e936d51be90ce5c466c5a655627f31f1b7
Mark Mossberg <mark.mossberg@gmail.com>
2016-02-18 12:18:38 -0500

    add ultrasound

--- src/pic16_usound/main.c
+++ src/pic16_usound/main.c
@@ -32,12 +32,28 @@ void main(void)
     /* Initialize I/O and Peripherals for application */
     InitApp();
     TRISC0 = 0;
+    
+    TRISC1 = 1; // set pin as input
+    ANSC1 = 1; // select pin as analog in
+    ADCON0 = 0b00010101;
+    ADCON1 = 0b11110000;
+    ADCON2 = 0b00000000;
+    //ADCS = 0b11;
+    //CHS = 5;
     while(1)
     {
-        RC0 = 1;
-        __delay_ms(500);  // 1 second delay 
-    RC0=0;                    // make RD7 pin Low to Off LED 
-    __delay_ms(500);
+        
+        ADCON0bits.GO_nDONE = 1;
+        while (ADCON0bits.GO);
+        int x = ((ADRESH * 256) + ADRESL);
+        if (x > 50) {
+            RC0 = 1;
+        }else {
+             RC0 = 0;
+        }
+        //__delay_ms(500);  // 1 second delay 
+   // RC0=0;                    // make RD7 pin Low to Off LED 
+   // __delay_ms(500);
         /* TODO <INSERT USER APPLICATION CODE HERE> */
     }
 
