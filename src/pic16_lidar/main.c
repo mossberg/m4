e9cdad03faa6584f3a5892eb8971f63c5423af37
Mark Mossberg <mark.mossberg@gmail.com>
2016-02-23 13:37:10 -0500

    [broken] lidar: initial attempt at getting i2c going

--- src/pic16_lidar/main.c
+++ src/pic16_lidar/main.c
@@ -21,6 +21,150 @@
 
 /* i.e. uint8_t <variable_name>; */
 
+/*
+ * I2C Primitives
+ */
+
+/*
+Function: I2CInit
+Return:
+Arguments:
+Description: Initialize I2C in master mode, Sets the required baudrate
+*/
+void I2CInit(void)
+{
+	TRISC0 = 1;      /* SDA and SCL as input pin */
+	TRISC1 = 1;      /* these pins can be configured either i/p or o/p */
+    /* maybe replace this with an assignment */
+	SSP1STAT |= 0x80; /* Slew rate disabled */
+	SSP1CON1 = 0x28;   /* SSPEN = 1, I2C Master mode, clock = FOSC/(4 * (SSPADD + 1)) */
+	SSP1ADD = 0x28;    /* 100Khz @ 4Mhz Fosc */
+}
+ 
+/*
+Function: I2CStart
+Return:
+Arguments:
+Description: Send a start condition on I2C Bus
+*/
+void I2CStart()
+{
+	SEN = 1;         /* Start condition enabled */
+	while(SEN);      /* automatically cleared by hardware */
+                     /* wait for start condition to finish */
+}
+ 
+/*
+Function: I2CStop
+Return:
+Arguments:
+Description: Send a stop condition on I2C Bus
+*/
+void I2CStop()
+{
+	PEN = 1;         /* Stop condition enabled */
+	while(PEN);      /* Wait for stop condition to finish */
+                     /* PEN automatically cleared by hardware */
+}
+ 
+/*
+Function: I2CRestart
+Return:
+Arguments:
+Description: Sends a repeated start condition on I2C Bus
+*/
+void I2CRestart()
+{
+	RSEN = 1;        /* Repeated start enabled */
+	while(RSEN);     /* wait for condition to finish */
+}
+ 
+/*
+Function: I2CAck
+Return:
+Arguments:
+Description: Generates acknowledge for a transfer
+*/
+void I2CAck()
+{
+	ACKDT = 0;       /* Acknowledge data bit, 0 = ACK */
+	ACKEN = 1;       /* Ack data enabled */
+	while(ACKEN);    /* wait for ack data to send on bus */
+}
+ 
+/*
+Function: I2CNck
+Return:
+Arguments:
+Description: Generates Not-acknowledge for a transfer
+*/
+void I2CNak()
+{
+	ACKDT = 1;       /* Acknowledge data bit, 1 = NAK */
+	ACKEN = 1;       /* Ack data enabled */
+	while(ACKEN);    /* wait for ack data to send on bus */
+}
+ 
+/*
+Function: I2CWait
+Return:
+Arguments:
+Description: wait for transfer to finish
+*/
+void I2CWait()
+{
+	while ((SSPCON2 & 0x1F ) || ( SSPSTAT & 0x04 ) );
+    /* wait for any pending transfer */
+}
+ 
+/*
+Function: I2CSend
+Return:
+Arguments: dat - 8-bit data to be sent on bus
+           data can be either address/data byte
+Description: Send 8-bit data on I2C bus
+*/
+void I2CSend(unsigned char dat)
+{
+	SSPBUF = dat;    /* Move data to SSPBUF */
+	while(BF);       /* wait till complete data is sent from buffer */
+	I2CWait();       /* wait for any pending transfer */
+}
+ 
+/*
+Function: I2CRead
+Return:    8-bit data read from I2C bus
+Arguments:
+Description: read 8-bit data from I2C bus
+*/
+unsigned char I2CRead(void)
+{
+	unsigned char temp;
+/* Reception works if transfer is initiated in read mode */
+	RCEN = 1;        /* Enable data reception */
+	while(!BF);      /* wait for buffer full */
+	temp = SSPBUF;   /* Read serial buffer and store in temp register */
+	I2CWait();       /* wait to check any pending transfer */
+	return temp;     /* Return the read data from bus */
+}
+
+/*
+ * I2C Abstraction Layer
+ * modeled off of arduino
+ */
+
+/* uint8_t i2c_write(uint8_t device_addr, uint8_t reg_addr, uint8_t data) */
+/* { */
+/*     uint8_t ret = 0; */
+
+/*     // trigger start condition on bus */
+/*     I2CStart(); */
+    
+/* } */
+
+
+
+
 /******************************************************************************/
 /* Main Program                                                               */
 /******************************************************************************/
@@ -31,26 +175,20 @@ void main(void)
 
     /* Initialize I/O and Peripherals for application */
     InitApp();
-    TRISC0 = 0;
+TRISC2 = 0;
+    I2CInit();
     
-    TRISC1 = 1; // set pin as input
-    ANSC1 = 1; // select pin as analog in
-    ADCON0 = 0b00010101;
-    ADCON1 = 0b11110000;
-    ADCON2 = 0b00000000;
-    //ADCS = 0b11;
-    //CHS = 5;
     while(1)
     {
+        I2CStart();
+          I2CSend(0x55); 
+          I2CStop();
+          
+          RC2 = 1;
+        __delay_ms(100);  // 1 second delay 
+        RC2 = 0;
+        __delay_ms(100);  // 1 second delay 
         
-        ADCON0bits.GO_nDONE = 1;
-        while (ADCON0bits.GO);
-        int x = ((ADRESH * 256) + ADRESL);
-        if (x > 50) {
-            RC0 = 1;
-        }else {
-             RC0 = 0;
-        }
         //__delay_ms(500);  // 1 second delay 
    // RC0=0;                    // make RD7 pin Low to Off LED 
    // __delay_ms(500);
