a08339cbee0d9f2e4e4a528df036b2989c0d4448
Mark Mossberg <mark.mossberg@gmail.com>
2016-03-01 17:51:56 -0500

    pic16_blink: blink on rc2 since that's where the led is now

--- src/pic16_blink.X/main.c
+++ src/pic16_blink.X/main.c
@@ -31,13 +31,13 @@ void main(void)
 
     /* Initialize I/O and Peripherals for application */
     InitApp();
-    TRISC0 = 0;
+    TRISC2 = 0;
     while(1)
     {
-        RC0 = 1;
-        __delay_ms(500);  // 1 second delay 
-    RC0=0;                    // make RD7 pin Low to Off LED 
-    __delay_ms(500);
+        RC2 = 1;
+        __delay_ms(50);  // 1 second delay 
+    RC2=0;                    // make RD7 pin Low to Off LED 
+    __delay_ms(50);
         /* TODO <INSERT USER APPLICATION CODE HERE> */
     }
 
